efb871e927acbf82f154b9510303b2a6623f1a01
punit <mashruwalapunit@gmail.com>
2021-08-24 13:10:17 -0400

    minor change

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ app.use(express.urlencoded({
 }));
 
 app.use(fileupload());
+
+
+
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://caustator.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
