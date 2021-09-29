83d333ffb909fdd9239e255a90b019b0bf52bc6f
punit <mashruwalapunit@gmail.com>
2021-09-28 22:56:21 -0400

    fixes

--- index.js
+++ index.js
@@ -9,22 +9,14 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 10010;
 
+app.options('*', cors())
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
 app.use(fileupload());
-
-
-
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
 
 app.use("/annotate", uploadRouter)
 
